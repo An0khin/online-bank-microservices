5aa634c7ea31a8f0b935271dbc14e2e83e5e9bad
An0khin <anohin04@mail.ru>
2023-01-10 10:24:51 +0500

    Microservice architecture and EurekaServer

---
+++ spring-microservice-eureka-naming-server/src/main/java/com/home/config/DispatcherServlet.java
@@ -0,0 +1,20 @@
+package com.home.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
