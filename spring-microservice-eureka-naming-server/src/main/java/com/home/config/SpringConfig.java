c33014805c96dce0e76bae091df371f883408df5
An0khin <anohin04@mail.ru>
2023-01-19 22:53:51 +0500

    Working eureka server on spring boot

--- spring-microservice-eureka-naming-server/src/main/java/com/home/config/SpringConfig.java
+++ spring-microservice-eureka-naming-server/src/main/java/com/home/config/SpringConfig.java
@@ -1,49 +1,13 @@
 package com.home.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring6.SpringTemplateEngine;
-import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
-@Configuration
-@EnableWebMvc
+@SpringBootApplication
 @EnableEurekaServer
-public class SpringConfig implements WebMvcConfigurer {
-    @Autowired
-    ApplicationContext applicationContext;
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
-        thymeleafViewResolver.setTemplateEngine(templateEngine());
-        registry.viewResolver(thymeleafViewResolver);
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine engine = new SpringTemplateEngine();
-
-        engine.setEnableSpringELCompiler(true);
-        engine.setTemplateResolver(resourceTemplateResolver());
-
-        return engine;
-    }
-
-    @Bean
-    public SpringResourceTemplateResolver resourceTemplateResolver() {
-        SpringResourceTemplateResolver resourceTemplateResolver = new SpringResourceTemplateResolver();
-
-        resourceTemplateResolver.setApplicationContext(applicationContext);
-        resourceTemplateResolver.setPrefix("/WEB-INF/views");
-        resourceTemplateResolver.setSuffix(".html");
-
-        return resourceTemplateResolver;
+public class SpringConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfig.class, args);
     }
 }
