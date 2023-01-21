375de979f618bff380452e9d8d961287b88958f4
An0khin <anohin04@mail.ru>
2023-01-21 15:41:43 +0500

    View debit card by id

--- spring-microservice-debit-transactions/src/main/java/com/home/config/SpringConfig.java
+++ spring-microservice-debit-transactions/src/main/java/com/home/config/SpringConfig.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @ComponentScan("com.home.*")
 @EntityScan("com.home.*")
 @EnableJpaRepositories(basePackages = "com.home.repository")
+@EnableWebMvc
 public class SpringConfig {
     public static void main(String[] args) {
         SpringApplication.run(SpringConfig.class, args);
--- spring-microservice-debit-transactions/src/main/java/com/home/controller/DebitController.java
+++ spring-microservice-debit-transactions/src/main/java/com/home/controller/DebitController.java
@@ -4,16 +4,16 @@ import com.home.model.DebitCard;
 import com.home.repository.DebitCardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
-
-@RestController
+@Controller
 @RequestMapping("/debit-client")
-public class Controller {
+public class DebitController {
     @Value("${eureka.instance.instance-id}")
     String instance;
 
@@ -21,12 +21,15 @@ public class Controller {
     DebitCardRepository debitCardRepository;
 
     @GetMapping("/test")
+    @ResponseBody
     public String test() {
         return "Hello on " + instance;
     }
 
     @GetMapping("/debit")
-    public DebitCard debits(@RequestParam("id") int id) {
-        return debitCardRepository.findById(id).orElse(null);
+    public String debits(@RequestParam("id") int id,
+                         Model model) {
+        model.addAttribute("card", debitCardRepository.findById(id).orElse(null));
+        return "debitCards/view";
     }
 }
