97e33d70da434084adc1ebeeb7154cc86e23ca74
An0khin <anohin04@mail.ru>
2023-03-09 11:38:20 +0500

    Credit cards controller

--- spring-microservice-credit-transactions/src/main/java/com/home/controller/CreditController.java
+++ spring-microservice-credit-transactions/src/main/java/com/home/controller/CreditController.java
@@ -1,13 +1,17 @@
 package com.home.controller;
 
+import com.home.model.CreditCard;
+import com.home.model.CreditLoan;
+import com.home.model.CreditRequest;
 import com.home.service.CreditDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/credit")
@@ -20,59 +24,140 @@ public class CreditController {
     static final String URL = "http://localhost:8082";
 
 
-//    @GetMapping("/newCreditRequest")
-//    public String newCreditRequestPage(Model model) {
-//
-//        model.addAttribute("creditRequest", new CreditRequest());
-//
-//        return "creditCards/newCreditRequest";
-//    }
-//
-//    @PostMapping("/newCreditRequest")
-//    public String newCreditRequest(@ModelAttribute("creditRequest") CreditRequest creditRequest,
-//                                   HttpServletRequest request) {
-//        Account account = accountDAO.findAccountByLogin(request.getUserPrincipal().getName());
-//        creditRequest.setBorrower(account);
-//
-//        cardDAO.saveCreditRequest(creditRequest);
-//
-//        return "redirect:/";
-//    }
-//
-//    @GetMapping("/takeCredit")
-//    public String takeCreditPage(Model model,
-//                                 HttpServletRequest request) {
-//
-//        Account account = accountDAO.findAccountByLogin(request.getUserPrincipal().getName());
-//
-//        model.addAttribute("creditCards", account.getCreditCards());
+    @GetMapping("/accrue")
+    @ResponseBody
+    public Boolean accrue(@RequestParam("id") int id,
+                          @RequestParam("money") double money) {
+        return creditDAO.accrueMoneyById(id, money);
+    }
+
+    @GetMapping("/take")
+    @ResponseBody
+    public Boolean take(@RequestParam("id") int id,
+                        @RequestParam("money") double money) {
+        return creditDAO.takeMoneyById(id, money);
+    }
+
+    @GetMapping("/get")
+    @ResponseBody
+    public CreditCard getById(@RequestParam("id") int id) {
+        return creditDAO.findCreditCardById(id);
+    }
+
+    @GetMapping("/get_all")
+    @ResponseBody
+    public List<CreditCard> getAllByAccountId(@RequestParam("accountId") String accountId) {
+        return creditDAO.findAllCreditCardsByAccountId(accountId);
+    }
+
+    @GetMapping("/order_new")
+    public String createNewCreditCardPage(Model model) {
+        model.addAttribute("flag", "");
+
+        return "creditCards/createNew";
+    }
+
+    @PostMapping("/order_new")
+    public String createNewCreditCard(@RequestParam(value = "agree", required = false, defaultValue = "false") boolean agree,
+                                      Model model) {
+        if(agree) {
+            String accountId = SecurityContextHolder.getContext().getAuthentication().getName(); //Find current user's id from another service
+
+            creditDAO.saveCreditCard(new CreditCard(accountId));
+
+            return "redirect:" + URL;
+        } else {
+            model.addAttribute("flag", "You haven't read the agreement!");
+            return "creditCards/createNew";
+        }
+    }
+
+    @GetMapping("/view")
+    public String viewCredit(@RequestParam("id") int id,
+                             Model model) {
+        model.addAttribute("card", creditDAO.findCreditCardById(id));
+        return "creditCards/view";
+    }
+
+    @GetMapping("/transfer/to_debit")
+    public String transferCreditToDebit(Model model) {
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        model.addAttribute("creditCards", creditDAO.findAllCreditCardsByAccountId(accountId));
+
+        return "creditCards/transfer";
+    }
+
+    @PostMapping("/transfer/to_debit")
+    public String transferCreditToDebitPost(@RequestParam("from") int from,
+                                            @RequestParam("to") int to,
+                                            @RequestParam("money") double money) {
+
+        CreditCard creditFrom = creditDAO.findCreditCardById(from);
+
+        if(creditFrom.takeMoney(money)) {
+            restTemplate.getForObject(URL + "/debit/accrue?id={id}&money={money}",
+                    Boolean.class,
+                    to, money);
+            creditDAO.saveCreditCard(creditFrom);
+        }
+
+        return "redirect:" + URL;
+    }
+
+    @GetMapping("/newCreditRequest")
+    public String newCreditRequestPage(Model model) {
+
+        model.addAttribute("creditRequest", new CreditRequest());
+
+        return "creditCards/newCreditRequest";
+    }
+
+    @PostMapping("/newCreditRequest")
+    public String newCreditRequest(@ModelAttribute("creditRequest") CreditRequest creditRequest) {
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        creditRequest.setBorrower(accountId);
+
+        creditDAO.saveCreditRequest(creditRequest);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/takeCredit")
+    public String takeCreditPage(Model model) {
+
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        model.addAttribute("creditCards", creditDAO.findAllCreditCardsByAccountId(accountId));
 //        model.addAttribute("id", new Text());
 //        model.addAttribute("money", new Number());
-//
-//        return "creditCards/takeCredit";
-//    }
-//
-//    @PostMapping("/takeCredit")
-//    public String takeCredit(@ModelAttribute("id") Text id,
-//                             @ModelAttribute("money") Number money,
-//                             BindingResult result) {
-//
-//        CreditCard creditCard = cardDAO.findCreditCardById(Integer.valueOf(id.getText()));
-//
-//        if(transactionDAO.findAllNotClosedCreditLoansByCreditCardId(creditCard.getId()) != 0) {
-//            return "creditCards/alreadyHasCredit";
-//        }
-//
-//        if(!creditCard.takeCreditMoney(money.getNumber())) {
+
+        return "creditCards/takeCredit";
+    }
+
+    @PostMapping("/takeCredit")
+    public String takeCredit(@RequestParam("id") int id,
+                             @RequestParam("money") double money,
+                             Model model) {
+
+        CreditCard creditCard = creditDAO.findCreditCardById(id);
+
+        if(creditDAO.findAllNotClosedCreditLoansByCreditCardId(creditCard.getId()) != 0) {
+            return "creditCards/alreadyHasCredit";
+        }
+
+        if(!creditCard.takeCreditMoney(money)) {
 //            result.addError(new FieldError("money", "number", "Must be less or equals " + creditCard.getMoneyLimit()));
-//            return "creditCards/takeCredit";
-//        }
-//
-//        transactionDAO.saveCreditLoan(new CreditLoan(creditCard, money.getNumber()));
-//        cardDAO.saveCreditCard(creditCard);
-//
-//        return "redirect:/";
-//    }
+            model.addAttribute("flag", "Must be less or equals " + creditCard.getMoneyLimit());
+            return "creditCards/takeCredit";
+        }
+
+        creditDAO.saveCreditLoan(new CreditLoan(creditCard, money));
+        creditDAO.saveCreditCard(creditCard);
+
+        return "redirect:/";
+    }
 //
 //
 //    //Read the data
@@ -86,14 +171,6 @@ public class CreditController {
 //        return "creditCards/allCreditCards";
 //    }
 //
-//    @GetMapping("/creditCard")
-//    public String viewCreditCard(@RequestParam("id") Integer id,
-//                                 Model model) {
-//        model.addAttribute("card", cardDAO.findCreditCardById(id));
-//
-//        return "creditCards/view";
-//    }
-//
 //    @GetMapping("/allCreditRequests")
 //    public String allCreditRequests(Model model,
 //                                    HttpServletRequest request) {
@@ -105,30 +182,5 @@ public class CreditController {
 //        model.addAttribute("notViewed", cardDAO.findNotViewedCreditRequestsByAccountId(id));
 //
 //        return "creditCards/allCreditRequests";
-//    }
-//
-//    @GetMapping("/transfer/creditToDebit")
-//    public String transferCreditToDebitPage(Model model, HttpServletRequest request) {
-//        Account account = accountDAO.findAccountByLogin(request.getUserPrincipal().getName());
-//
-//        model.addAttribute("debitCards", cardDAO.findAllDebitCardsByAccountId(account.getId()));
-//        model.addAttribute("credits", cardDAO.findAllCreditCardsByAccountId(account.getId()));
-//
-//        model.addAttribute("ids", new Text());
-//        model.addAttribute("money", new Number());
-//
-//        return "creditCards/transferToDebit";
-//    }
-//
-//    @PostMapping("/transfer/creditToDebit")
-//    public String transferCreditToDebit(@ModelAttribute("ids") Text ids, @ModelAttribute("money") Number money) {
-//        String[] strings = ids.getText().split(",");
-//
-//        DebitCard to = cardDAO.findDebitCardById(Integer.valueOf(strings[1]));
-//        CreditCard from = cardDAO.findCreditCardById(Integer.valueOf(strings[0]));
-//
-//        cardDAO.transferMoneyFromTo(from, to, money.getNumber().doubleValue());
-//
-//        return "redirect:/";
 //    }
 }
--- spring-microservice-credit-transactions/src/main/java/com/home/service/CreditDAO.java
+++ spring-microservice-credit-transactions/src/main/java/com/home/service/CreditDAO.java
@@ -1,4 +1,38 @@
 package com.home.service;
 
+import com.home.model.CreditCard;
+import com.home.model.CreditLoan;
+import com.home.model.CreditRequest;
+
+import java.util.List;
+
 public class CreditDAO {
+    public List<CreditCard> findAllCreditCardsByAccountId(String accountId) {
+        return null;
+    }
+
+    public CreditCard findCreditCardById(int id) {
+        return null;
+    }
+
+    public void saveCreditLoan(CreditLoan creditLoan) {
+    }
+
+    public void saveCreditCard(CreditCard creditCard) {
+    }
+
+    public int findAllNotClosedCreditLoansByCreditCardId(Integer id) {
+        return 0;
+    }
+
+    public void saveCreditRequest(CreditRequest creditRequest) {
+    }
+
+    public boolean takeMoneyById(int id, double money) {
+        return false;
+    }
+
+    public boolean accrueMoneyById(int id, double money) {
+        return false;
+    }
 }
