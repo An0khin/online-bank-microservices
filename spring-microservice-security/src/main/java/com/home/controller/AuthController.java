27be64011d4db0db5068b93030b554e605e3f19e
An0khin <anohin04@mail.ru>
2023-03-16 16:56:06 +0500

    Changed urls

--- spring-microservice-security/src/main/java/com/home/controller/AuthController.java
+++ spring-microservice-security/src/main/java/com/home/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/auth")
+@RequestMapping("/token")
 @RestController
 @Slf4j
 public class AuthController {
