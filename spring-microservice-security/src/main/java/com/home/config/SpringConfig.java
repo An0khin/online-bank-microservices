5349a2883be2d9efdce55e2ac1e84c6ddbe44f57
An0khin <anohin04@mail.ru>
2023-03-17 00:47:49 +0500

    Deleted unused things

--- spring-microservice-security/src/main/java/com/home/config/SpringConfig.java
+++ spring-microservice-security/src/main/java/com/home/config/SpringConfig.java
@@ -2,10 +2,8 @@ package com.home.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,8 +12,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @ComponentScan("com.home")
-@EnableJpaRepositories("com.home.repository")
-@EntityScan("com.home")
 @EnableWebMvc
 @EnableWebSecurity
 public class SpringConfig {
